d7f35958117359697b6f04203beecd90676f15fe
unknown <chikakool2010@gmail.com>
2022-10-08 08:10:28 +0100

    added some files

--- script.js
+++ script.js
@@ -1,74 +1,98 @@
-window.addEventListener('load', () => {
-    const form = document.querySelector("#new-task-form");
-    const input = document.querySelector("#new-task-input");
-    const list_el = document.querySelector("#task");
+// DOM ELEMENTS
+const time = document.getElementById('time'),
+   greeting = document.getElementById('greeting'),
+   name = document.getElementById('name'),
+   focus = document.getElementById('focus');
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-
-        const task = input.value;
-
-        if (!task) {
-            alert("please fill out the task");
-            return;
-        }    
-        
-        const task_el = document.createElement("div");
-        task_el.classList.add("task");
-
-       const task_content_el = document.createElement("div");
-        task_content_el.classList.add("content");
-        
-        task_el.appendChild(task_content_el);
-
-
-        
-
-        const task_input_el = document.createElement("input");
-        task_input_el.classList.add("text");
-        task_input_el.type = "text";
-        task_input_el.value = task;
-        task_input_el.setAttribute("readonly", "readonly");
-
-        task_content_el.appendChild(task_input_el);
-
-        const task_actions_el = document.createElement("div");
-        task_actions_el.classList.add("actions");
-
-        const task_edit_el = document.createElement("button");
-        task_edit_el.classList.add("edit");
-
-        const task_delete_el = document.createElement("button");
-        task_delete_el.classList.add("delete");
-        task_delete_el.innerHTML = "delete";
-
-        task_actions_el.appendChild(task_edit_el);
-        task_actions_el.appendChild(task_delete_el);
-
-        task_el.appendChild(task_actions_el);
-
-        list_el.appendChild(task_el);
-
-        input.value = "";
-
-        task_edit_el.addEventListener('click', () => {
-           if (task_edit_el.innerText.toLocaleLowerCase() == "edit") {
-
-                task_input_el.removeAttribute("readonly");
-                task_input_el.focus();
-                task_edit_el.innerText = "save";
-
-           } else {
-               task_input_el.setAttribute("readonly", "readonly");
-               task_edit_el.innerText = "Edit";
-           }
-    })   
+// Options
+const showAmPm = true;   
+// Show Time
+function showTime() {
+   let today = new Date(),
+    hour = today.getHours(),
+    min = today.getMinutes(),
+    sec = today.getSeconds();
     
-    task_delete_el.addEventListener('click', () => {
-        list_el.removeChild(task_el);
-    });
-});    
-});
+    // Set AM or PM
+    const ampm = hour >= 12 ? 'PM' : 'AM';
+
+    // 12hr Format
+    hour = hour % 12 || 12;
+
+    // Output Time
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
+      sec
+    )} ${showAmPm ? ampm : ''}`;  
+    setTimeout(showTime, 1000)
+}
+
+// Add Zero
+function addZero(n) {
+   return (parseInt(n, 10) < 10 ? '0' : '') + n;
+
+}
+
+// Set Background and Greeting
+function setBgGreet() {
+   let today = new Date(),
+   hour = today.getHours();
+
+if(hour < 12) {
+   // Morning
+   document.body.style.backgroundImage = "url('../img/morning.jpg')";
+   greeting.textContent = 'Good Morning';
+} else if(hour < 18) {
+   // Afternoon
+   document.body.style.backgroundImage = "url('../img/Afternoon.jpg') ";
+   greeting.textContent = 'Good Morning ';
+} else {
+   // Evening
+   document.body.style.backgroundImage = "url('../img/Night.jpg')";
+   greeting.textContent = 'Good Evening';
+   document.body.style.color = 'white';
+}
+}  
+
+// Get Name
+function getName() {
+   if(localStorage.getItem('name') === null) {
+      name.textContent = 'Edmund Chika';
+   } else {
+      name.textContent = localStorage.getItem('name');
+   }
+}
+
+// Set Name
+function setName(e) {
+   if(e.type === 'keypress') {
+      // make sure enter is pressed
+      if(e.which == 13 || e.keycode == 13)
+      localStorage.setItem('name', e.target.innerText);
+      name.blur();
+   } else {
+      localStorage.setItem('name', e.target.innerText);
+   }
+}
+
+// Get Focus
+function getFocus() {
+   if(localStorage.getItem('focus') === null) {
+      focus.textContent = '[Enter Focus]';
+   } else {
+      focus.textContent = localStorage.getItem('focus');
+   }
+}
+
+
+
+
+// Run
+showTime();
+setBgGreet();
+getName();   
+getFocus();
+
+
 
 
 
